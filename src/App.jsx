6ce6a3301bb7ab2ba20e2cbdbfe89c4c89ed4106
Dmitry Kazantsev <dmitry_kazantsev@yahoo.com>
2024-12-12 23:06:25 +0300

    Initial UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+//import './App.css';
 
-function App() {}
+const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
 
-export default App;
+export default function App() {
+  const step = 3;
+
+  function handlePrevious() {
+    alert('Previous');
+  }
+
+  function handleNext() {
+    alert('Next');
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step === 3 ? 'active' : ''}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+//export default App;
