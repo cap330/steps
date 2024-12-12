27caa3f9b456f375323952df6d686b1834191c7b
Dmitry Kazantsev <dmitry_kazantsev@yahoo.com>
2024-12-13 00:06:46 +0300

    BAD PRACTICE

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # React + Vite
 
 React project
+
+npm: package.json
+set "start": "vite --port 3000 --open",
+
+const [step, setStep] = useState(1)
--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,31 @@ const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new 
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: 'Dmitry' });
 
   function handlePrevious() {
-    setStep(step - 1);
+    if (step > 1) setStep(step - 1);
   }
 
   function handleNext() {
-    setStep(step + 1);
+    if (step < 3) setStep(step + 1);
+
+    // BAD PRACTICE
+    //test.name = 'Fred';
+    setTest({ name: 'Fred' });
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step === 3 ? 'active' : ''}`}>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step === 3 ? 'active' : ''}>3</div>
       </div>
 
       <p className="message">
         Step {step}: {messages[step - 1]}
+        {test.name}
       </p>
 
       <div className="buttons">
